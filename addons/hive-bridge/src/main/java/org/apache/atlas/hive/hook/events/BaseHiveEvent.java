f3ae45a1b92b5f6910e45d45b4e497cce9f7bc3c
congyh <yihao.cong@outlook.com>
2019-04-03 16:08:46 +0800

    WIP rewrite origin column name

--- addons/hive-bridge/src/main/java/org/apache/atlas/hive/hook/events/BaseHiveEvent.java
+++ addons/hive-bridge/src/main/java/org/apache/atlas/hive/hook/events/BaseHiveEvent.java
@@ -87,6 +87,7 @@ public abstract class BaseHiveEvent {
     public static final String S3A_SCHEME               = "s3a" + SCHEME_SEPARATOR;
 
     public static final String ATTRIBUTE_QUALIFIED_NAME            = "qualifiedName";
+    // Note: TODO: 从这里下手, 找到列名的所有地方.
     public static final String ATTRIBUTE_NAME                      = "name";
     public static final String ATTRIBUTE_DESCRIPTION               = "description";
     public static final String ATTRIBUTE_OWNER                     = "owner";
@@ -488,6 +489,7 @@ public abstract class BaseHiveEvent {
 
                 column.setAttribute(ATTRIBUTE_TABLE, tableId);
                 column.setAttribute(ATTRIBUTE_QUALIFIED_NAME, colQualifiedName);
+                // Note: TODO: 列名改写
                 column.setAttribute(ATTRIBUTE_NAME, fieldSchema.getName());
                 column.setAttribute(ATTRIBUTE_OWNER, table.getOwner());
                 column.setAttribute(ATTRIBUTE_COL_TYPE, fieldSchema.getType());
